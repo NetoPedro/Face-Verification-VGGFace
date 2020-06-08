a857454ab74f2354e2439f4094ce3b4f7380510c
NetoPedro <pedroneto_09@hotmail.com>
2020-06-08 14:52:18 +0300

    Added an identity layer class

--- model.py
+++ model.py
@@ -1,7 +1,19 @@
 from torch import nn
+import torch
+from torchvision import models
+
 class TinyFaces(nn.Module):
     pass
 
 
 class FaceFeatureExtractor(nn.Module):
     pass
+
+
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+
+    def forward(self, x):
+        return x
+
