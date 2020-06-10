e7e64ad824ee409c8f622d36a64cd5366d1f487b
NetoPedro <pedroneto_09@hotmail.com>
2020-06-10 21:40:11 +0300

    Added the code to conver the model

--- model_converter.py
+++ model_converter.py
@@ -2,6 +2,8 @@ from torch import nn
 import torch
 from torchvision import models
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 class Identity(nn.Module):
     def __init__(self):
         super(Identity, self).__init__()
@@ -9,27 +11,24 @@ class Identity(nn.Module):
     def forward(self, x):
         return x
 
-class VGGFace_training(nn.Module):
-    def __init__(self,identities=1000):
-        super(ResNet50ToTrain,self).__init__()
+class VGGFace_Extractor(nn.Module):
+    def __init__(self):
+        super(VGGFace_Extractor,self).__init__()
         self.model = models.resnet50(True)
         self.model.fc = Identity()
         self.model.avgpool = Identity()
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(2048,identities)
         self.embed_layer = nn.Linear(2048,1024)
 
-    def forward(self, x,triplet = False):
+    def forward(self, x):
         x = self.model(x).reshape(x.shape[0],-1,7,7)
         out = self.avgpool(x)
-        if not(triplet):
-            out = self.fc(out.flatten(start_dim=1))
-        else:
-            out = out.reshape(out.shape[0], -1)
-            x = out / torch.norm(out, 2,dim=1).reshape(out.shape[0],1)
-            embedding = self.embed_layer(x)
-            return out,embedding
-        return  out, x
+
+        out = out.reshape(out.shape[0], -1)
+        x = out / torch.norm(out, 2,dim=1).reshape(out.shape[0],1)
+        embedding = self.embed_layer(x)
+        return out,embedding
+
 
     def triplet_mode(self):
         for param in self.model.parameters():
@@ -38,3 +37,19 @@ class VGGFace_training(nn.Module):
 
     def save_weights(self,path):
         torch.save(self.state_dict(), path)
+
+def convert_model(path="model.mdl"):
+    new_model = VGGFace_Extractor()
+
+    pretrained_dict = torch.load(path, map_location=lambda storage, loc: storage)
+    model_dict = new_model.state_dict()
+
+    pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+
+    model_dict.update(pretrained_dict)
+
+    new_model.load_state_dict(pretrained_dict)
+
+    new_model.save_weights("face_extractor_model.mdl")
+
+convert_model()
